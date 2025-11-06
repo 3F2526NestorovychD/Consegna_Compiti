93cbb7b4424ba6177a4fa40278118595820d9e54
Dima_PC_Scolastico <nestorovych.dmytro.2023@itisgalileiroma.it>
2025-11-06 09:52:46 +0100

    bubble sort - da finire

---
+++ lab-06_11_2025/11/bubble_sort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int man(){
+    // dichiarazioni
+    int n;
+    double num;
+    
+
+    cout << "Inserisca la lungezza della lista da riordinare";
+    cin >> n;
+    int vettore[n];
+
+for(int i = 0; i < n; i++){
+    
+    cin >> vettore[i];
+}
+
+
+
+   return 0; 
+}
