61b980098af3548615db35adbd22ad583cbf9fee
PC_Domestico <nestorovych.dmytro.2023@itisgalileiroma.it>
2025-11-24 18:01:57 +0100

    Inizio i compiti per 24-11-2025

---
+++ 2025-11-25_Compiti/pag89.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+void controllo()
+{
+	char nome[15];
+	int eta;
+	printf ("Inserire il nome: ");
+	
+	scanf("%s", &nome);
+	printf ("Inserire l'eta': ");
+	scanf("%d", &eta);
+	if (eta >= 18)
+	printf("%s e' maggiorenne\n",nome);
+    else
+    printf("%s e' minorenne\n",nome);
+
+}
+
+int main() {
+
+	controllo();
+	system("pause");
+	return 0;
+}
+
