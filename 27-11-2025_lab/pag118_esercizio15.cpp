106dfc20c50393b1b49fcfcc23ae80b0b9e6e029
PC Istituzionale <ciruzzo@email.it>
2025-11-27 09:07:37 +0100

    Finisco il primo programmino

--- 27-11-2025_lab/pag118_esercizio15.cpp
+++ 27-11-2025_lab/pag118_esercizio15.cpp
@@ -3,11 +3,37 @@
 
 using namespace std;
 
+void CalcSpedizipne(float pessino, float tariffa, float costino)
+{
+  if(pessino <= 20)
+    {
+         cout << "\nIl pacco non ha costi aggiuntivi poiche pessa meno di 20kg[" << pessino << "kg]" << endl;
+       
+    }
+    else
+    {
+         float kgSopra = pessino - 20; 
+         float costoExtra = kgSopra * (costino * tariffa);
+         float costoFinale = costoExtra + costino;
+
+         cout << "\nIl costo della spedizione è: " << costoFinale << "€ \n" << "insieme al costo aggiuntivo che è: " << costoExtra << "€" << endl;
+    }
+
+}
 
 
-int main{
+int main() {
+   float pesso, prezzoBiglieto; 
+   float tarif =  0.02; // 2% per kg sopra i 20kg
 
+    cout << "Inserisca il peso del pacco in kg: ";
+    cin >> pesso;
+    cout << "Inserisc il prezzo del biglieto: ";
+    cin >> prezzoBiglieto;
 
-      
+    
+    CalcSpedizipne(pesso, tarif, prezzoBiglieto);
+
+    system("pause");  
     return 0;
 }
