36863036467a9778bc3dd903fc36314db5450103
PC_Domestico <nestorovych.dmytro.2023@itisgalileiroma.it>
2025-12-01 18:31:27 +0100

    Finisco il primo programmino

---
+++ 02-12-2025_Compiti/pag117_es1.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void SegnoNumero()
+{
+  float num;
+
+    printf ("Inserisca il numero da verificare: ");
+    scanf ("%f", &num);
+    if (num>0)
+      printf("il numero %.3f e' positivo\n", num);
+     else if(num<0)
+      printf("Il suo numerino %.3f e' negativo" , num) ;
+     else
+      printf("Il numero e' neutro o nullo\n");
+}
+
+
+  int main() {
+
+    SegnoNumero();
+
+    system("pause");
+    return 0;
+}
+
+   
