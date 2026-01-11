03bb9c85acc436834f495b2bfa9d690558dd4283
PC_domestico <nestorovych.dmytro.2023@itisgalileiroma.it>
2026-01-11 18:42:24 +0100

    Piccolla ottimizazione

--- 2026-01-12_Compiti/pag156_es1.c
+++ 2026-01-12_Compiti/pag156_es1.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    int n, x, min;
+    int n;
+    int numeri[100];  
+    int min, max;
 
     printf("Quanti numeri vuole inserire? ");
     scanf("%d", &n);
 
 
-    scanf("%d", &x);
-    min = x; // ecco la risposta
-
+    for (int i = 0; i < n; i++) {
+        numeri[i] = rand() % 100;  
+        printf("Numero [%d]: %d\n", i+1, numeri[i]);
+    }
  
+    min = numeri[0];
+    max = numeri[0];
+
     for (int i = 1; i < n; i++) {
-        scanf("%d", &x);
-        if (x < min) {
-            min = x;
-        }
+        if (numeri[i] < min) min = numeri[i];
+        if (numeri[i] > max) max = numeri[i];
     }
 
-    printf("Il valore minimo è: %d\n", min);
+    printf("Valore minimo: %d\n", min);
+    printf("Valore massimo: %d\n", max);
+    printf("Differenza massimo-minimo: %d\n", max - min);
 
+    system("pause");
     return 0;
 }
