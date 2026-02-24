5ab5d640d7866af5a553f1ab3e0c931684780cc1
PC_Domestico <nestorovych.dmytro.2023@itisgalileiroma.it>
2026-02-24 21:11:16 +0100

    Cambio nome dello screenashot per pisozionamento corretto, ottimizazioni generali del codice

--- 2026-02-25_Compiti/pag308_es1.c
+++ 2026-02-25_Compiti/pag308_es1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define N 3
 #define M 4
 int TotRiga(int m[N][M], int r);
@@ -11,48 +12,48 @@ int main(){
     
     int mat[N][M] = {{2,3,4,5}, {6,7,8,9}, {10,11,12,13}};
 
-    int i, j, tot, totr, totc;
-    for (i = 0; i < N; i++){
+    int tot, totr, totc;
+    for (int i = 0; i < N; i++){
         totr = TotRiga(mat, i);
         printf("Totale riga %d = %d\n", i+1, totr);
     }
 
-    for (j = 0; j < M; j++){
+    for (int j = 0; j < M; j++){
         totc = TotColonna(mat, j);
         printf("Totale colonna %d = %d\n", j+1, totc);
     }
 
     tot = Totale(mat);
     printf("Totale generale = %d\n", tot);
-    
-    system("PAUSE");
+
+    system("pause");
     return 0;
 }
 
 
 
 int TotRiga(int m[N][M], int r){
-    int j, tot;
-    tot = 0;
-    for (j = 0; j < M; j++)
-        tot = tot + m[r][j];
-    return tot;
+    int totale;
+    totale = 0;
+    for (int j = 0; j < M; j++)
+        totale += m[r][j];
+    return totale;
 }
 
 int TotColonna(int m[N][M], int c){
-    int i, tot;
-    tot = 0;
-    for (i = 0; i < N; i++)
-        tot = tot + m[i][c];
-    return tot;
+    int totale;
+    totale = 0;
+    for (int i = 0; i < N; i++)
+        totale +=  m[i][c];
+    return totale;
 }
 
 int Totale(int m[N][M]){
-    int j, i, tot;
-    tot = 0;
-    for (i = 0; i < N; i++)
-        for (j = 0; j < M; j++)
-            tot = tot + m[i][j];
+    int totale;
+    totale = 0;
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++)
+            totale += m[i][j];
 
-    return tot;
+    return totale;
 }
