e6907a0aed918e689bd8b4cb19f7702e23241600
PC_Domestico <nestorovych.dmytro.2023@itisgalileiroma.it>
2026-06-02 01:02:33 +0200

    Correzione svista di struttura codice
    è stata rilevato che il main si trovava nella fine del codice, il che rende i prototipo inutili(che sono stati creati qui solo per la leggibilita)

--- 2026-05-28_lab/SelfCheck.c
+++ 2026-05-28_lab/SelfCheck.c
@@ -22,116 +22,6 @@ int    maggVoto(Studente array[], int lun);
 int    numSufficienze(Studente array[], int lun);
 int    ricerca(Studente array[], int lun, const char cognome[]);
 
-
-void menu() {
-    printf(  "\n\nGESTIONE CLASSE\n");
-    printf(  "1) Stampa studenti\n");
-    printf(  "2) Media classe\n");
-    printf(  "3) Studente con voto piu alto\n");
-    printf(  "4) Numero sufficienze\n");
-    printf(  "5) Ricerca per cognome\n");
-    printf(  "0) Esci\n");
-    printf("Scelta: ");
-}
-
-
-int chiedi_stud() {
-    int num;
-    do {
-        printf("Inserisca il numero di studenti da inserire (max %d): ",
-               MAX_STUD);
-        if (scanf("%d", &num) != 1 || num <= 0 || num > MAX_STUD) {
-            printf("Valore non valido, riprova.\n");
-            while (getchar() != '\n');
-            continue;
-        }
-        break;
-    } while (1);
-    return num;
-}
-
-int inserimento_array(Studente array[], int lun) {
-    for (int i = 0; i < lun; i++) {
-        printf("\n\nStudente %d\n", i + 1);
-        printf("Inserisca Nome Cognome Voto: ");
-        if (scanf("%19s %19s %f", array[i].nome, array[i].cognome, &array[i].voto) != 3) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
-void sort(Studente array[], int lun) {
-    for (int i = 0; i < lun - 1; i++)
-        for (int j = 0; j < lun - 1 - i; j++)
-            if (strcmp(array[j].cognome, array[j + 1].cognome) > 0) {
-                Studente tmp  = array[j];
-                array[j]  = array[j + 1];
-                array[j + 1]  = tmp;
-            }
-}
-
-void stampa(Studente array[], const int LUN) {
-    printf("\n=== LISTA DELLA CLASSE ===\n");
-    for (int i = 0; i < LUN; i++) {
-        printf("%-3d Nome: %-15s Cognome: %-15s Voto: %.2f\n",
-               i + 1, array[i].nome, array[i].cognome, array[i].voto);
-    }
-}
-
-void stampa_studente(Studente array[], const int LUN, const int idx) {
-    if (idx < 0 || idx >= LUN) {
-        printf("Indice non valido.\n");
-        return;
-    }
-    printf("\n=== STUDENTE ===\n");
-    printf("Nome:\t%s\n", array[idx].nome);
-    printf("Cognome:\t%s\n", array[idx].cognome);
-    printf("Voto:\t%.2f\n", array[idx].voto);
-}
-
-void stampa_media(Studente array[], const int LUN) {
-    float media = 0;
-    for (int i = 0; i < LUN; i++)
-        media += array[i].voto;
-    media /= LUN;
-    printf("\n=== MEDIA CLASSE ===\n");
-    printf("Media: %.2f\n", media);
-}
-
-int maggVoto(Studente array[], const int LUN) {
-    int idx = 0;
-    for (int i = 1; i < LUN; i++)
-        if (array[i].voto > array[idx].voto)
-            idx = i;
-    return idx;
-}
-
-int numSufficienze(Studente array[], const int LUN) {
-    int num = 0;
-    for (int i = 0; i < LUN; i++)
-        if (array[i].voto >= 6)
-            num++;
-    return num;
-}
-
-int ricerca(Studente array[], int lun, const char cognome[]) {
-    sort(array, lun);   
-
-    int left = 0, right = lun - 1;
-
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        int cmp = strcmp(array[mid].cognome, cognome);
-        if      (cmp == 0) return mid;
-
-        if (cmp  < 0) left = mid + 1;
-        else          
-        right = mid - 1;
-    }
-    return -1;
-}
-
 int main() {
 
     const int NUM_STUDENTI = chiedi_stud();
@@ -196,4 +86,114 @@ int main() {
     } while (scelta != 0);
 
     return 0;
+}
+
+
+void menu() {
+    printf(  "\n\nGESTIONE CLASSE\n");
+    printf(  "1) Stampa studenti\n");
+    printf(  "2) Media classe\n");
+    printf(  "3) Studente con voto piu alto\n");
+    printf(  "4) Numero sufficienze\n");
+    printf(  "5) Ricerca per cognome\n");
+    printf(  "0) Esci\n");
+    printf("Scelta: ");
+}
+
+
+int chiedi_stud() {
+    int num;
+    do {
+        printf("Inserisca il numero di studenti da inserire (max %d): ",
+               MAX_STUD);
+        if (scanf("%d", &num) != 1 || num <= 0 || num > MAX_STUD) {
+            printf("Valore non valido, riprova.\n");
+            while (getchar() != '\n');
+            continue;
+        }
+        break;
+    } while (1);
+    return num;
+}
+
+int inserimento_array(Studente array[], int lun) {
+    for (int i = 0; i < lun; i++) {
+        printf("\n\nStudente %d\n", i + 1);
+        printf("Inserisca Nome Cognome Voto: ");
+        if (scanf("%19s %19s %f", array[i].nome, array[i].cognome, &array[i].voto) != 3) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void sort(Studente array[], int lun) {
+    for (int i = 0; i < lun - 1; i++)
+        for (int j = 0; j < lun - 1 - i; j++)
+            if (strcmp(array[j].cognome, array[j + 1].cognome) > 0) {
+                Studente tmp  = array[j];
+                array[j]  = array[j + 1];
+                array[j + 1]  = tmp;
+            }
+}
+
+void stampa(Studente array[], const int LUN) {
+    printf("\n=== LISTA DELLA CLASSE ===\n");
+    for (int i = 0; i < LUN; i++) {
+        printf("%-3d Nome: %-15s Cognome: %-15s Voto: %.2f\n",
+               i + 1, array[i].nome, array[i].cognome, array[i].voto);
+    }
+}
+
+void stampa_studente(Studente array[], const int LUN, const int idx) {
+    if (idx < 0 || idx >= LUN) {
+        printf("Indice non valido.\n");
+        return;
+    }
+    printf("\n==== STUDENTE ====\n");
+    printf("Nome:\t%s\n", array[idx].nome);
+    printf("Cognome:\t%s\n", array[idx].cognome);
+    printf("Voto:\t%.2f\n", array[idx].voto);
+}
+
+void stampa_media(Studente array[], const int LUN) {
+    float media = 0;
+    for (int i = 0; i < LUN; i++)
+        media += array[i].voto;
+    media /= LUN;
+    printf("\n=== MEDIA CLASSE ===\n");
+    printf("Media: %.2f\n", media);
+}
+
+int maggVoto(Studente array[], const int LUN) {
+    int idx = 0;
+    for (int i = 1; i < LUN; i++)
+        if (array[i].voto > array[idx].voto)
+            idx = i;
+    return idx;
+}
+
+int numSufficienze(Studente array[], const int LUN) {
+    int num = 0;
+    for (int i = 0; i < LUN; i++)
+        if (array[i].voto >= 6)
+            num++;
+    return num;
+}
+
+int ricerca(Studente array[], int lun, const char cognome[]) {
+    sort(array, lun);   
+
+    int left = 0, right = lun - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        int cmp = strcmp(array[mid].cognome, cognome);
+        if      (cmp == 0) return mid;
+
+        if (cmp  < 0) left = mid + 1;
+        else          
+        right = mid - 1;
+    }
+    return -1;
 }
